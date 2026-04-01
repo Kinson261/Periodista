e11eb60da6333b4d123d108936f7243d3638f37a
kinson261 <clarkrazananirina13@gmail.com>
2026-04-01 18:04:00 +0300

    [MAINT] Reinforce Typing in `parserURL.py`

--- src/parserURL.py
+++ src/parserURL.py
@@ -1,6 +1,9 @@
-def parseURLFileUserInput(input: str):
-    file = open(input, "r")
-    lines = []
+from typing import List
+
+
+def parseURLFileUserInput(input_file: str):
+    file = open(input_file, "r")
+    lines: List[str] = []
     for line in file:
         lines.append(line.strip())
     file.close()
